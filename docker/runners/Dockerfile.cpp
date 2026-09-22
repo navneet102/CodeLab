6fc9f3c23fe3722be69b3c36fda46213b02503bf
Navneet <conavneet@gmail.com>
2026-09-22 09:06:48 +0530

    Remove rundundant images

--- docker/runners/Dockerfile.cpp
+++
@@ -1,8 +0,0 @@
-FROM gcc:13-bookworm
-
-RUN groupadd runner && useradd -g runner runner
-
-WORKDIR /code
-RUN chown runner:runner /code
-
-USER runner
--- docker/runners/Dockerfile.java
+++
@@ -1,8 +0,0 @@
-FROM eclipse-temurin:21-jdk-alpine
-
-RUN addgroup -S runner && adduser -S runner -G runner
-
-WORKDIR /code
-RUN chown runner:runner /code
-
-USER runner
